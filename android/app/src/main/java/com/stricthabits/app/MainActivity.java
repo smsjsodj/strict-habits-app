1d510f9db6018c8f5d39f581733a18b5b4b8d2ad
smsjs <smsjsodj@gmail.com>
2026-05-10 11:48:52 +0200

    add fullscreen and locked mode

---
+++ android/app/src/main/java/com/stricthabits/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.stricthabits.app;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Показывать поверх блокировки экрана и включать экран
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+}
